1d1f3057c5f17414f1c86236f2dcd4653aaf1dbe
joegoggin <joegoggindev@gmail.com>
2025-06-27 04:12:55 -0400

    Update `PostPreview` To Actually Show `first_paragraph`

--- src/components/post_preview.rs
+++ src/components/post_preview.rs
@@ -4,13 +4,13 @@ use crate::models::post::Post;
 
 #[component]
 pub fn PostPreview(post: Post) -> impl IntoView {
-    let first_paragraph = post.get_paragraphs().clone();
+    let first_paragraph = post.get_paragraphs()[0].clone();
 
     view! {
         <div class="post-preview">
             <h3>{post.title.clone()}</h3>
             <h5>{post.get_date()}</h5>
-            <p>{first_paragraph.len()}</p>
+            <p>{first_paragraph}</p>
             <button>Read More</button>
         </div>
     }
